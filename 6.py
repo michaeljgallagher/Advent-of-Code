9fba6ce65fec52f2be3a37f75d3be44a25be4ed7
MG <11604739+michaeljgallagher@users.noreply.github.com>
2019-12-06 17:11:47 -0500

    Finish day 6 part 2

--- 6.py
+++ 6.py
@@ -3,14 +3,49 @@ with open('6.txt', 'r') as data:
 data = data[:-1]
 orbits = {x[1]: x[0] for x in data}
 
-total = 0
-for planet in orbits.keys():
-    curr = orbits[planet]
+
+# PART 1
+
+def count_paths(orbits):
+    total = 0
+    for planet in orbits.keys():
+        curr = orbits[planet]
+        while True:
+            total += 1
+            if curr == 'COM':
+                break
+            curr = orbits[curr]
+    return total
+
+
+print(count_paths(orbits))  # 142497
+
+
+# PART 2
+
+def find_path(origin, orbits):
+    total = 0
+    curr = orbits[origin]
+    planets = []
     while True:
         total += 1
         if curr == 'COM':
             break
-        if curr in orbits.keys():
-            curr = orbits[curr]
+        planets.append(curr)
+        curr = orbits[curr]
+    return planets
 
-print(total)
+
+def find_intersection(path1, path2):
+    for x in path2:
+        if x in path1:
+            i = path1.index(x)
+            k = path2.index(x)
+            intersect = path1[:i] + path2[k:0:-1]
+            break
+    return intersect
+
+
+my_path = find_path('YOU', orbits)
+santa_path = find_path('SAN', orbits)
+print(len(find_intersection(my_path, santa_path)))  # 301
