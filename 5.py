114e15ea106489c2315d2b7f92a6198bf75b6b1d
MG <11604739+michaeljgallagher@users.noreply.github.com>
2019-12-08 15:54:42 -0500

    redo day 5 to redo day 7

--- 5.py
+++ 5.py
@@ -1,69 +1,67 @@
-class Intcode:
-    def __init__(self, intcode):
-        self.intcode = intcode
-        self.copy = self.intcode.copy()
-        self.index = 0
-
-        self.modes = {'0': lambda x: self.read(x),  # position mode
-                      '1': lambda x: x}             # immediate mode
-
-        self.opcodes = {'01': lambda x, y, out: self.write(x + y, out),
-                        '02': lambda x, y, out: self.write(x * y, out),
-                        '03': lambda x: self.write(int(input('System ID: ')), x),
-                        '04': lambda x: print('Diagnostic Code: {}'.format(x)),
-                        '05': lambda x, y: self.jump(index=y) if x else None,
-                        '06': lambda x, y: self.jump(index=y) if not x else None,
-                        '07': lambda x, y, out: self.write(int(x < y), out),
-                        '08': lambda x, y, out: self.write(int(x == y), out),
-                        '99': None}
-
-    def read(self, index=None):
-        if index is None:
-            index = self.index
-            self.index += 1
-        return self.copy[index]
-
-    def jump(self, index):
-        self.index = index
-
-    def write(self, value, index=None):
-        if index is None:
-            index = self.index
-        self.copy[index] = value
-
-    def padding(self, s, opcode):
-        param_counts = {'01': 3,
-                        '02': 3,
-                        '03': 1,
-                        '04': 1,
-                        '05': 2,
-                        '06': 2,
-                        '07': 3,
-                        '08': 3}
-        num_params = param_counts[opcode]
-
-        if opcode in ['04', '05', '06']:
-            return s.zfill(num_params)
-        return '1' + s.zfill(num_params-1)
-
-    def run(self):
-        self.index = 0
-        self.copy = self.intcode.copy()
-        while True:
-            raw = str(self.read())
-            opcode = raw[-2:].zfill(2)
-            instruction = self.opcodes[opcode]
-            if instruction is None:
-                break
-            else:
-                modes = reversed(self.padding(raw[:-2], opcode))
-                params = (self.modes[mode](self.read()) for mode in modes)
-                instruction(*params)
-        return self.index
-
-
-with open('5.txt', 'r') as data:
+with open('5.txt') as data:
     data = list(map(int, data.read().split(',')))
 
-test = Intcode(data)
-test.run()
+ops = {
+        1: lambda x, y: x+y,
+        2: lambda x, y: x*y,
+        7: lambda x, y: 1 if x < y else 0,
+        8: lambda x, y: 1 if x == y else 0
+        }
+
+
+def find_modes(cmd):
+    mode1 = mode2 = mode3 = 0
+    cmd, params = cmd % 100, cmd // 100
+    mode1 = params % 10
+    params //= 10
+    mode2 = params % 10
+    params //= 10
+    mode3 = params % 10
+    return cmd, mode1, mode2, mode3
+
+
+def intcode(program, inp):
+    print(f'Input = {inp}')
+    pointer = 0
+    output = int
+
+    while True:
+        cmd = program[pointer]
+        mode1 = mode2 = mode3 = 0
+
+        if cmd == 99:
+            return output
+
+        if cmd > 100:
+            cmd, mode1, mode2, mode3 = find_modes(cmd)
+
+        if cmd in [1, 2, 5, 6, 7, 8]:
+            p1 = program[pointer + 1] if mode1 else program[program[pointer + 1]]
+            p2 = program[pointer + 2] if mode2 else program[program[pointer + 2]]
+            index = program[pointer + 3]  # parameter3, always in position mode (mode3 is redundant and unused)
+            if cmd in [1, 2, 7, 8]:
+                program[index] = ops[cmd](p1, p2)
+                pointer += 4
+            elif cmd == 5:
+                pointer = p2 if p1 else pointer+3
+            elif cmd == 6:
+                pointer = pointer+3 if p1 else p2
+
+        elif cmd == 3:
+            program[program[pointer+1]] = inp
+            pointer += 2
+
+        elif cmd == 4:
+            index = pointer+1 if mode1 else program[pointer+1]
+            output = program[index]
+            pointer += 2
+
+        else:
+            return -1  # error
+
+
+# Part 1
+print(intcode(data[:], 1))  # 5577461
+
+# Part 2
+print(intcode(data[:], 5))  # 7161591
