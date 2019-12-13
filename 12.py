1f2fbfde03fee0c51782714ab5b8a95487f58f69
MG <11604739+michaeljgallagher@users.noreply.github.com>
2019-12-12 21:39:09 -0500

    refactor to make part 2 easier (hopefully)

--- 12.py
+++ 12.py
@@ -1,48 +1,48 @@
-'''
-<x=-3, y=10, z=-1>
-<x=-12, y=-10, z=-5>
-<x=-9, y=0, z=10>
-<x=7, y=-5, z=-3>
-'''
-
 from itertools import combinations
+import re
+import numpy as np
 
-io = [[-3, 10, -1], [0, 0, 0]]
-europa = [[-12, -10, -5], [0, 0, 0]]
-ganymede = [[-9, 0, 10], [0, 0, 0]]
-callisto = [[7, -5, -3], [0, 0, 0]]
-
-
-def gravity(moon1, moon2):
-    for i in range(3):
-        if moon1[0][i] < moon2[0][i]:
-            moon1[1][i] += 1
-            moon2[1][i] -= 1
-        if moon1[0][i] > moon2[0][i]:
-            moon1[1][i] -= 1
-            moon2[1][i] += 1
+with open('12.txt', 'r') as data:
+    data = data.read().strip().split('\n')
 
 
-def apply_velocity(moon):
-    for i in range(3):
-        moon[0][i] += moon[1][i]
+def parse(data):
+    regex = re.compile(r'<x=(.*), y=(.*), z=(.*)>')
+    return [list(map(int, regex.findall(data[i])[0])) for i in range(len(data))]
 
 
-def time_step():
-    moons = [io, europa, ganymede, callisto]
-    for moon1, moon2 in combinations(moons, 2):
-        gravity(moon1, moon2)
-    for moon in moons:
-        apply_velocity(moon)
+def gravity(positions):
+    new_positions = [[0 for _ in range(3)] for _ in range(4)]
+    for i, j in combinations(range(4), 2):
+        for k in range(3):
+            if positions[i][k] < positions[j][k]:
+                new_positions[i][k] += 1
+                new_positions[j][k] -= 1
+            if positions[i][k] > positions[j][k]:
+                new_positions[i][k] -= 1
+                new_positions[j][k] += 1
+    return new_positions
 
 
-def total_energy(moon):
-    pe = sum([abs(x) for x in moon[0]])
-    ke = sum([abs(x) for x in moon[1]])
-    return pe * ke
+def energy(positions, velocities):
+    total = 0
+    for i in range(4):
+        total += sum([abs(x) for x in positions[i]]) * sum([abs(x) for x in velocities[i]])
+    return total
 
 
-# Part 1
-for i in range(1000):
-    time_step()
-print(sum([total_energy(moon) for moon in [io, europa, ganymede, callisto]]))  # 10944
+def time_step(positions, velocities):
+    d_v = gravity(positions)
+    for i in range(4):
+        for j in range(3):
+            velocities[i][j] += d_v[i][j]
+    for i in range(4):
+        for j in range(3):
+            positions[i][j] += velocities[i][j]
+
+
+positions = parse(data)
+velocities = [[0 for _ in range(3)] for _ in range(4)]
+for _ in range(1000):
+    time_step(positions, velocities)
+print(energy(positions, velocities))  # 10944
