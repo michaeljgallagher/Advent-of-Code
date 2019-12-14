bec6713c485ff9f80e1b0865b2384204565ffbd2
MG <11604739+michaeljgallagher@users.noreply.github.com>
2019-12-14 14:21:31 -0500

    day 14 part 2

--- 14.py
+++ 14.py
@@ -17,12 +17,12 @@ def parse(data):
     return reactions
 
 
-def ore_needed(reactions):
+def ore_needed(reactions, fuel_wanted):
     stock = {k: 0 for k in reactions.keys()}
     needed = {k: 0 for k in reactions.keys()}
-    needed['FUEL'] = reactions['FUEL'][0]
+    needed['FUEL'] = reactions['FUEL'][0] * fuel_wanted
     stock['ORE'] = needed['ORE'] = 0
-    amount_needed = reactions['FUEL'][0]
+    amount_needed = reactions['FUEL'][0] * fuel_wanted
 
     while amount_needed:
         for ingr in needed:
@@ -43,5 +43,21 @@ def ore_needed(reactions):
     return needed['ORE']
 
 
+def max_fuel(reactions, amount_ore):
+    ore_for_one_fuel = ore_needed(reactions, 1)
+    lower = amount_ore // ore_for_one_fuel
+    upper = lower * 2
+    fuel = lower
+    while lower < upper:
+        middle = (upper + lower) // 2
+        if ore_needed(reactions, middle) < amount_ore:
+            lower = middle + 1
+            fuel = middle
+        else:
+            upper = middle - 1
+    return fuel
+
+
 reactions = parse(data)
-print(f'Part 1: {ore_needed(reactions)}')  # 97422
+print(f'Part 1: {ore_needed(reactions, 1)}')  # 97422
+print(f'Part 2: {max_fuel(reactions, 1000000000000)}')  # 13108426
