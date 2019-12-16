7ef7c2f873601eea5497102a209d58cb224dc805
MG <11604739+michaeljgallagher@users.noreply.github.com>
2019-12-16 08:45:55 -0500

    day 16 part 2

--- 16.py
+++ 16.py
@@ -19,16 +19,19 @@ def fft2(number):
 
 
 # Part 1
-number = data
+number = data[:]
 for _ in range(100):
     number = fft2(number)
 res = ''.join(map(str, number[:8]))
 print(f'Part 1: {res}')  # 89576828
 
 # Part 2
-'''data = '03036732577212944063491565474664'
-offset = int(data[:7])
-number = data * 10000
+offset = int(''.join(map(str, data[:7])))
+number = (data * 10000)[offset:]
+length = len(number)
 for _ in range(100):
-    number = fft(number)
-print(f'Part 2: {number[offset:offset+8]}')'''
+    for i in range(length-2, -1, -1):
+        number[i] += number[i+1]
+        number[i] %= 10
+res = ''.join(map(str, number[:8]))
+print(f'Part 2: {res}')  # 23752579
