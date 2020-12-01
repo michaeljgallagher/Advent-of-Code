0ec02953e7a5a7b6581c4b19446e65b5777cab53
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-01 13:34:40 -0500

    add timing functionality

--- 2020/timer.py
+++ 2020/timer.py
@@ -4,10 +4,10 @@ import time
 def timer(func):
     @wraps(func)
     def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()    # 1
+        start_time = time.perf_counter()
         value = func(*args, **kwargs)
-        end_time = time.perf_counter()      # 2
-        run_time = end_time - start_time    # 3
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
         print(f"Finished {func.__name__!r} in {run_time:.6f} seconds")
         return value
     return wrapper_timer
