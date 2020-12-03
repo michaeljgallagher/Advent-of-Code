e9d2f5d23cafba8b3017fc29b7ba97d6bac92f7c
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-03 00:22:34 -0500

    refactor day 03

--- 2020/03.py
+++ 2020/03.py
@@ -1,28 +1,22 @@
-from collections import deque
-
-
 with open('03.txt', 'r') as file:
     data = file.read().split('\n')
 
 
 def part1(data, r, d):
-    n = len(data)
-    m = len(data[0])
+    n, m = len(data), len(data[0])
     j = 0
     res = 0
     for i in range(0, n, d):
-        cur = deque(data[i])
-        cur.rotate(-j)
-        if cur[0] == '#':
+        if data[i][j] == '#':
             res += 1
-        j += r
+        j = (j+r) % m
     return res
 
 
 
 def part2(data):
     res = 1
-    for r, d in [(1,1), (3,1), (5, 1), (7, 1), (1, 2)]:
+    for r, d in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
         res *= part1(data, r, d)
     return res
 
