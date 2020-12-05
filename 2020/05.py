ff6fd2360e9378624ffa6c81444c9c35855deaec
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-05 12:05:01 -0500

    golfing

--- 2020/05.py
+++ 2020/05.py
@@ -1,14 +1,6 @@
-with open('05.txt', 'r') as file:
-    data = file.read().split('\n')
-
-
 def convert(boarding_pass):
     table = str.maketrans('FBLR', '0101')
     return int(boarding_pass.translate(table), 2)
-
-
-def find_seats(data):
-    return set(convert(boarding_pass) for boarding_pass in data)
 
 
 def part_one(seats):
@@ -21,6 +13,8 @@ def part_two(seats):
 
 
 if __name__ == '__main__':
-    seats = find_seats(data)
+    with open('05.txt', 'r') as file:
+        seats = set(convert(boarding_pass) for boarding_pass in file.read().split('\n'))
+    
     print(f'Part 1: {part_one(seats)}')  # 959
     print(f'Part 2: {part_two(seats)}')  # 527
