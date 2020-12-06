1553106e3bf48cbee911075ee38b75c212274c81
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-06 02:04:35 -0500

    slight refactor

--- 2020/06.py
+++ 2020/06.py
@@ -2,11 +2,11 @@ with open('06.txt', 'r') as file:
     data = file.read().split('\n\n')
 
 def part_one(data):
-    return sum(len(set.union(*[set(list(s)) for s in group.split()])) for group in data)
+    return sum(len(set.union(*[set(s) for s in group.split()])) for group in data)
 
 
 def part_two(data):
-    return sum(len(set.intersection(*[set(list(s)) for s in group.split()])) for group in data)
+    return sum(len(set.intersection(*[set(s) for s in group.split()])) for group in data)
 
 
 print(f'Part 1: {part_one(data)}')  # 6775
