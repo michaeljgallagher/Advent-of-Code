820d422569dae6844ed263a12c1493463ed41f51
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-06 01:59:41 -0500

    golf part two

--- 2020/06.py
+++ 2020/06.py
@@ -1,21 +1,13 @@
-from string import ascii_lowercase as letters
-
 with open('06.txt', 'r') as file:
     data = file.read().split('\n\n')
 
 
 def part_one(data):
-    return sum(len(set(c for c in group if c !='\n')) for group in data)
+    return sum(len(set.union(*[set(list(s)) for s in group.split()])) for group in data)
 
 
 def part_two(data):
-    res = 0
-    for group in data:
-        cur = set(letters)
-        for s in group.split():
-            cur &= set(s)
-        res += len(cur)
-    return res
+    return sum(len(set.intersection(*[set(list(s)) for s in group.split()])) for group in data)
 
 
 print(f'Part 1: {part_one(data)}')  # 6775
