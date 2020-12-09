f8d5a4544d62e8e822928b17a1adda231f90c7de
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-08 21:34:54 -0500

    imporve performance of part two ( 0.338812 seconds --> 0.014592 seconds)

--- 2020/bootcode/__init__.py
+++ 2020/bootcode/__init__.py
@@ -1,15 +1,18 @@
+from collections import Counter
+
+
 class BootCode:
     def __init__(self, instructions):
         self.instructions = instructions
         self.accum = 0
         self.idx = 0
-        self.seen = set()
+        #self.seen = set()
         self.ops = {
             'acc': self.acc,
             'jmp': self.jmp,
             'nop': self.nop,
         }
-        self.halt = False
+        #self.halt = False
         self.length = len(self.instructions)
     
     def acc(self, val):
@@ -25,13 +28,45 @@ class BootCode:
     def cycle_check(self):
         if self.idx in self.seen:
             self.halt = True
+
+    def reset(self):
+        self.idx = 0
+        self.accum = 0
+
+
+    def repair(self):
+        instructions = self.instructions
+        swap = {'jmp': 'nop', 'nop': 'jmp'}
+        cycles = list(self.run()[1])
+        while cycles:
+            cur = cycles.pop()
+            op, val = instructions[cur]
+            if op in ('jmp', 'nop'):
+                instructions[cur] = [swap[op], val]
+            res, cycle = self.run()
+            if not cycle:
+                return res
+            instructions[cur] = [op, val]
+
+
+
     
     def step(self):
         op, val = self.instructions[self.idx]
-        self.seen.add(self.idx)
+        #self.seen.add(self.idx)
         self.ops[op](val)
-        self.cycle_check()
+        #self.cycle_check()
     
     def run(self):
-        while not self.halt and self.idx < self.length:
+        """ while not self.halt and self.idx < self.length:
+            self.step() """
+        self.reset()
+        instructions = self.instructions
+        cycles = set()
+        while True:
+            if self.idx == self.length:
+                return self.accum, None
+            if self.idx in cycles:
+                return self.accum, cycles
+            cycles.add(self.idx)
             self.step()
