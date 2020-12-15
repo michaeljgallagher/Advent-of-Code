ea069d8fbff114ed74b83fd274a056815c3ca8ce
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-15 18:14:26 -0500

    rename function van_eck

--- 2020/15.py
+++ 2020/15.py
@@ -1,6 +1,6 @@
 data = [12,1,16,3,11,0]
 
-def brute_force(data, end=2020):
+def van_eck(data, end=2020):
     i = len(data) - 1
     seen = {v: k for k, v in enumerate(data)}
     cur = data[-1]
@@ -13,11 +13,11 @@ def brute_force(data, end=2020):
 
 
 def part_one(data):
-    return brute_force(data, 2020)
+    return van_eck(data, 2020)
 
 
 def part_two(data):
-    return brute_force(data, 30000000)
+    return van_eck(data, 30000000)
 
 
 print(f'Part 1: {part_one(data)}')  # 1696
