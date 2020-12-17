58c4c052f8c118ac0fa2ecf686d656836de93646
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-17 10:18:15 -0500

    generalize solution for N dimensions

--- 2020/17.py
+++ 2020/17.py
@@ -14,9 +14,8 @@ data = parse_input(data)
 
 def find_active(data, dim=3):
     active = set()
-    N, M = len(data), len(data[0])
-    for i in range(N):
-        for j in range(M):
+    for i in range(len(data)):
+        for j in range(len(data[0])):
             if data[i][j] == '#':
                 active.add((i, j, *([0]*(dim-2))))
     return active
@@ -26,20 +25,18 @@ def find_neighbors(active, dim=3):
     neighbors = defaultdict(int)
     for cube in active:
         for delta in product([-1, 0, 1], repeat=dim):
-            if all([x == 0 for x in delta]):
+            if all(x == 0 for x in delta):
                 continue
             cur = tuple(x+dx for x, dx in zip(cube, delta))
             neighbors[cur] += 1
     return neighbors
 
 
-def run_cycle(active, dim=3):
+def step(active, dim=3):
     new_active = set()
     neighbors = find_neighbors(active, dim)
     for k, v in neighbors.items():
-        if v == 3:
-            new_active.add(k)
-        elif v == 2 and k in active:
+        if v == 3 or (v == 2 and k in active):
             new_active.add(k)
     return new_active
 
@@ -47,14 +44,14 @@ def run_cycle(active, dim=3):
 def part_one(data):
     active = find_active(data, 3)
     for _ in range(6):
-        active = run_cycle(active, 3)
+        active = step(active, 3)
     return len(active)
 
 
 def part_two(data):
     active = find_active(data, 4)
     for _ in range(6):
-        active = run_cycle(active, 4)
+        active = step(active, 4)
     return len(active)
 
 
