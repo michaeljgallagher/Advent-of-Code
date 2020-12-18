ddebf8307327200f461bee9fc76b61dcc9231fef
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-18 00:57:35 -0500

    hacky solution day 18

---
+++ 2020/18.py
@@ -0,0 +1,51 @@
+with open('18.txt', 'r') as file:
+    data = file.read()
+
+
+def parse_input(data):
+    data = [line for line in data.split('\n')]
+    return data
+
+
+data = parse_input(data)
+
+
+class N:
+    def __init__(self, n):
+        self.n = n
+    
+    def __add__(self, other):
+        return N(self.n + other.n)
+
+    def __sub__(self, other):
+        return N(self.n * other.n)
+
+    def __mul__(self, other):
+        return N(self.n + other.n)
+
+
+def eval_line(line):
+    for i in range(10):
+        line = line.replace(f'{i}', f'N({i})')
+    line = line.replace('*', '-')
+    return eval(line, {'N': N}).n
+
+
+def eval_line_two(line):
+    for i in range(10):
+        line = line.replace(f'{i}', f'N({i})')
+    line = line.replace('*', '-')
+    line = line.replace('+', '*')
+    return eval(line, {'N': N}).n
+
+
+def part_one(data):
+    return sum(eval_line(line) for line in data)
+
+
+def part_two(data):
+    return sum(eval_line_two(line) for line in data)
+
+
+print(f'Part 1: {part_one(data)}')  # 131076645626
+print(f'Part 2: {part_two(data)}')  # 109418509151782
