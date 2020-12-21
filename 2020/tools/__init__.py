39d84eb512654af3b0b417092e095b330bf8f665
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-21 10:37:22 -0500

    use Hopcroft-Karp

--- 2020/tools/__init__.py
+++ 2020/tools/__init__.py
@@ -17,6 +17,19 @@ def hk(graph):
     '''
     Hopcroft-Karp bipartite matching
     '''
+    def recurse(v):
+        if v in possible:
+            cur = possible[v]
+            del possible[v]
+            for u in cur:
+                if u in pred:
+                    pu = pred[u]
+                    del pred[u]
+                    if pu is remaining or recurse(pu):
+                        res[v] = u
+                        return True
+        return False
+
     res = {}
     for u, v in graph.items():
         for x in v:
@@ -24,44 +37,32 @@ def hk(graph):
                 res[x] = u
                 break
     while True:
-	    possible = {}
-	    remaining = []
-	    pred = {u: remaining for u in graph}
-	    for v in res:
-		    del pred[res[v]]
-	    layer = list(pred)
-	    while layer and not remaining:
-		    new_layer = {}
-		    for u in layer:
-			    for v in graph[u]:
-				    if v not in possible:
-					    new_layer.setdefault(v,[]).append(u)
-		    layer = []
-		    for v in new_layer:
-			    possible[v] = new_layer[v]
-			    if v in res:
-				    layer.append(res[v])
-				    pred[res[v]] = v
-			    else:
-				    remaining.append(v)
-	    if not remaining:
-		    unlayered = {}
-		    for u in graph:
-			    for v in graph[u]:
-				    if v not in possible:
-					    unlayered[v] = None
-		    return res
-
-	    def recurse(v):
-		    if v in possible:
-			    cur = possible[v]
-			    del possible[v]
-			    for u in cur:
-				    if u in pred:
-					    pu = pred[u]
-					    del pred[u]
-					    if pu is remaining or recurse(pu):
-						    res[v] = u
-						    return True
-		    return False
-	    for v in remaining: recurse(v)
+        possible = {}
+        remaining = []
+        pred = {u: remaining for u in graph}
+        for v in res:
+            del pred[res[v]]
+        layer = list(pred)
+        while layer and not remaining:
+            new_layer = {}
+            for u in layer:
+                for v in graph[u]:
+                    if v not in possible:
+                        new_layer.setdefault(v,[]).append(u)
+            layer = []
+            for v in new_layer:
+                possible[v] = new_layer[v]
+                if v in res:
+                    layer.append(res[v])
+                    pred[res[v]] = v
+                else:
+                    remaining.append(v)
+        if not remaining:
+            unlayered = {}
+            for u in graph:
+                for v in graph[u]:
+                    if v not in possible:
+                        unlayered[v] = None
+            return res
+        else:
+            for v in remaining: recurse(v)
