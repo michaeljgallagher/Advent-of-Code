a84015f0d18f9e27d7e06b20b5d50189973147bd
MG <11604739+michaeljgallagher@users.noreply.github.com>
2020-12-22 09:35:13 -0500

    minor refactors

--- 2020/22.py
+++ 2020/22.py
@@ -23,24 +23,29 @@ def part_one(p1, p2):
 
 def part_two(p1, p2):
     def recurse(p1, p2, seen):
-        while (p1 and p2):
+        while p1 and p2:
             if (tuple(p1), tuple(p2)) in seen:
                 return 1, p1
+
             seen.add((tuple(p1), tuple(p2)))
+
             c1, c2 = p1.popleft(), p2.popleft()
             if c1 <= len(p1) and c2 <= len(p2):
                 sp1, sp2 = deque(list(p1)[:c1]), deque(list(p2)[:c2])
                 winner, _ = recurse(sp1, sp2, set())
             else:
                 winner = 1 if c1 > c2 else 0
+
             if winner == 1:
                 p1 += deque([c1, c2])
             else:
                 p2 += deque([c2, c1])
+
         return (1, p1) if p1 else (0, p2)
-    
+
     winner = recurse(p1, p2, set())[1]
     return sum((len(winner) - i) * v for i, v in enumerate(winner))
+
 
 p1, p2 = parse_input(data)
 print(f'Part 1: {part_one(p1, p2)}')  # 33473
