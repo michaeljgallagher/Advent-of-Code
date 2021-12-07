51d17e7bbd4a9594723236c7ffc10c510692c1ec
MG <11604739+michaeljgallagher@users.noreply.github.com>
2021-12-07 00:20:10 -0500

    fix formatting

--- 2021/07.py
+++ 2021/07.py
@@ -1,29 +1,30 @@
-with open('07.txt', 'r') as file:
+with open("07.txt", "r") as file:
     raw_data = file.read()
 
 
 def parse_input(raw_data):
-    return list(map(int, raw_data.split(',')))
+    return list(map(int, raw_data.split(",")))
 
 
 data = parse_input(raw_data)
 
 
 def part_one(data):
-    res = float('inf')
+    res = float("inf")
     for i in range(max(data)):
-        res = min(res, sum(abs(x-i) for x in data))
+        res = min(res, sum(abs(x - i) for x in data))
     return res
 
 
 def part_two(data):
     def gauss(n):
-        return (n*(n+1))//2
-    res = float('inf')
+        return (n * (n + 1)) // 2
+
+    res = float("inf")
     for i in range(max(data)):
-        res = min(res, sum(gauss(abs(x-i)) for x in data))
+        res = min(res, sum(gauss(abs(x - i)) for x in data))
     return res
 
 
-print(f'Part 1: {part_one(data)}')  # 335330
-print(f'Part 2: {part_two(data)}')  # 92439766
+print(f"Part 1: {part_one(data)}")  # 335330
+print(f"Part 2: {part_two(data)}")  # 92439766
