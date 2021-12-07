5ef03346608d8bd9bd699c5089bd36d3068d88b5
MG <11604739+michaeljgallagher@users.noreply.github.com>
2021-12-07 00:06:09 -0500

    finish day 7 quick

---
+++ 2021/07.py
@@ -0,0 +1,30 @@
+with open('07.txt', 'r') as file:
+    raw_data = file.read()
+
+
+def parse_input(raw_data):
+    return list(map(int, raw_data.split(',')))
+
+
+data = parse_input(raw_data)
+print(len(data))
+
+
+def part_one(data):
+    res = float('inf')
+    for i in range(max(data)):
+        res = min(res, sum(abs(x-i) for x in data))
+    return res
+
+
+def part_two(data):
+    def gauss(n):
+        return (n*(n+1))//2
+    res = float('inf')
+    for i in range(max(data)):
+        res = min(res, sum(gauss(abs(x-i)) for x in data))
+    return res
+
+
+print(f'Part 1: {part_one(data)}')  # 
+print(f'Part 2: {part_two(data)}')  # 
