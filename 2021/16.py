e586bf9d3abde44f20062dc0d8560a35640a3cd3
MG <11604739+michaeljgallagher@users.noreply.github.com>
2021-12-16 13:09:32 -0500

    Adjust some formatting

--- 2021/16.py
+++ 2021/16.py
@@ -16,31 +16,31 @@ def parse_literal(s, i):
     ni = i
     res = []
     while s[ni] == '1':
-        res.append(s[ni+1:ni+5])
+        res.append(s[ni + 1 : ni + 5])
         ni += 5
-    res.append(s[ni+1:ni+5])
+    res.append(s[ni + 1 : ni + 5])
     ni += 5
     return ni, int(''.join(res), 2)
 
 
 def parse_packet(s, i):
-    version, tid = int(s[i:i+3], 2), int(s[i+3:i+6], 2)
+    version, tid = int(s[i : i + 3], 2), int(s[i + 3 : i + 6], 2)
     VERSIONS.append(version)
     if tid == 4:
-        ni, val = parse_literal(s, i+6)
+        ni, val = parse_literal(s, i + 6)
         return ni, val
 
     values = []
-    ltid = s[i+6]
+    ltid = s[i + 6]
     val = 0
     if ltid == '0':
-        total_len = int(s[i+7:i+22], 2)
+        total_len = int(s[i + 7 : i + 22], 2)
         ni = i + 22
         while ni < i + 22 + total_len:
             ni, val = parse_packet(s, ni)
             values.append(val)
     else:  # ltid == '1'
-        packets = int(s[i+7:i+18], 2)
+        packets = int(s[i + 7 : i + 18], 2)
         ni = i + 18
         for _ in range(packets):
             ni, val = parse_packet(s, ni)
@@ -49,7 +49,7 @@ def parse_packet(s, i):
     if tid == 0:
         val = sum(values)
     elif tid == 1:
-        val = reduce(lambda x, y: x*y, values)
+        val = reduce(lambda x, y: x * y, values)
     elif tid == 2:
         val = min(values)
     elif tid == 3:
