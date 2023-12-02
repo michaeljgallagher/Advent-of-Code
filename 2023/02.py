fc2db3cf4dae92012a49d6f0859580144aff3112
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2023-12-02 08:45:08 -0500

    Use regex for parsing 2023 day 02

--- 2023/02.py
+++ 2023/02.py
@@ -1,3 +1,4 @@
+import re
 from collections import Counter, defaultdict
 from math import prod
 
@@ -6,40 +7,24 @@ with open("02.txt", "r") as file:
 
 
 def parse_game(game):
-    res = []
-    sets = game.split(":")[1].strip()
-    for set in sets.split(";"):
-        c = Counter()
-        for cubes in set.split(", "):
-            n, cur = cubes.strip().split(" ")
-            c[cur] = int(n)
-        res.append(c)
-    return res
+    cubes = defaultdict(int)
+    for n, color in re.findall(r"(\d+) (\w+)", game):
+        cubes[color] = max(cubes[color], int(n))
+    return cubes
 
 
-def is_possible(game, bag):
-    sets = parse_game(game)
-    for set in sets:
-        if any(v > 0 for v in (set - bag).values()):
-            return False
-    return True
+def is_possible(game):
+    bag = Counter({"red": 12, "green": 13, "blue": 14})
+    cubes = Counter(parse_game(game))
+    return not any(v > 0 for v in (cubes - bag).values())
 
 
 def find_power(game):
-    cubes = defaultdict(int)
-    sets = parse_game(game)
-    for set in sets:
-        for color in ("red", "green", "blue"):
-            cubes[color] = max(cubes[color], set[color])
-    return prod(cubes.values())
+    return prod(parse_game(game).values())
 
 
 def part_one(data):
-    return sum(
-        i
-        for i, game in enumerate(data, start=1)
-        if is_possible(game, Counter({"red": 12, "green": 13, "blue": 14}))
-    )
+    return sum(i for i, game in enumerate(data, start=1) if is_possible(game))
 
 
 def part_two(data):
