b9a81e6dad859a7b4d30cc5637bfa750aaffb1b7
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2023-12-12 01:29:28 -0500

    Add 2023 day 12

---
+++ 2023/12.py
@@ -0,0 +1,40 @@
+from functools import cache
+
+with open("12.txt", "r") as file:
+    data = file.read().strip()
+
+RECORDS = [
+    (x, tuple(map(int, y.split(","))))
+    for row in data.split("\n")
+    for x, y in [row.split(" ")]
+]
+
+
+@cache
+def dp(i, j, cur, seq, nums):
+    if i == len(seq):
+        return (j == len(nums) - 1 and nums[j] == cur) or (j == len(nums) and cur == 0)
+    res = 0
+    if seq[i] in "#?":
+        res += dp(i + 1, j, cur + 1, seq, nums)
+    if seq[i] in ".?":
+        if cur == 0:
+            res += dp(i + 1, j, 0, seq, nums)
+        elif cur > 0 and j < len(nums) and nums[j] == cur:
+            res += dp(i + 1, j + 1, 0, seq, nums)
+    return res
+
+
+def part_one():
+    return sum(dp(0, 0, 0, seq, nums) for seq, nums in RECORDS)
+
+
+def part_two():
+    unfolded = [
+        ("?".join(seq for _ in range(5)), tuple(nums * 5)) for seq, nums in RECORDS
+    ]
+    return sum(dp(0, 0, 0, seq, nums) for seq, nums in unfolded)
+
+
+print(f"Part 1: {part_one()}")  # 7670
+print(f"Part 2: {part_two()}")  # 157383940585037
