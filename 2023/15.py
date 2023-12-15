ffb4ff7b57a68feb379b99aac50d3301c548a954
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2023-12-15 08:40:51 -0500

    Use dictionary instead of a list for boxes since order is maintained

--- 2023/15.py
+++ 2023/15.py
@@ -1,50 +1,36 @@
+from functools import reduce
+
 with open("15.txt", "r") as file:
     data = file.read().strip()
 
 
-def hash(s):
-    res = 0
-    for c in s:
-        res = (res + ord(c)) * 17 % 256
-    return res
-
-
-def find_index(label, box):
-    for i, (lab, _) in enumerate(box):
-        if lab == label:
-            return i
-    return len(box)
+def h(s):
+    return reduce(lambda x, y: (x + ord(y)) * 17 % 256, s, 0)
 
 
 def hashmap(s, boxes):
     if "-" in s:
         label, _ = s.split("-")
-        k = hash(label)
-        i = find_index(label, boxes[k])
-        if i != len(boxes[k]):
-            boxes[k].pop(i)
+        k = h(label)
+        boxes[k].pop(label, 0)
     else:
         label, focal_len = s.split("=")
-        k = hash(label)
-        i = find_index(label, boxes[k])
-        if i != len(boxes[k]):
-            boxes[k][i] = (label, focal_len)
-        else:
-            boxes[k].append((label, focal_len))
+        k = h(label)
+        boxes[k][label] = focal_len
 
 
 def part_one():
-    return sum(hash(s) for s in data.split(","))
+    return sum(h(s) for s in data.split(","))
 
 
 def part_two():
-    boxes = [[] for _ in range(256)]
+    boxes = [{} for _ in range(256)]
     for s in data.split(","):
         hashmap(s, boxes)
     return sum(
         i * j * int(focal_len)
         for i, box in enumerate(boxes, start=1)
-        for j, (label, focal_len) in enumerate(box, start=1)
+        for j, (label, focal_len) in enumerate(box.items(), start=1)
     )
 
 
