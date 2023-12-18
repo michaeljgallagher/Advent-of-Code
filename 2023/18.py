e263c70b0413f12eb0331f1c67f66d8246e1253e
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2023-12-18 00:55:51 -0500

    Consolidate both parts into one solve function

--- 2023/18.py
+++ 2023/18.py
@@ -19,26 +19,18 @@ def shoelace_area(points):
     return abs(res) >> 1
 
 
-def part_one():
-    dirs = {"U": (-1, 0), "D": (1, 0), "R": (0, 1), "L": (0, -1)}
-    i, j = 0, 0
-    points = []
-    for d, n, _ in PLAN:
-        di, dj = dirs[d]
-        for _ in range(n):
-            i += di
-            j += dj
-            points.append((i, j))
-    return shoelace_area(points) + (len(points) >> 1) + 1
-
-
-def part_two():
+def solve(pt2=False):
+    dirs = dirs = (
+        [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        if pt2
+        else {"U": (-1, 0), "D": (1, 0), "R": (0, 1), "L": (0, -1)}
+    )
     i, j = 0, 0
     points = []
     perim = 0
-    for _, _, color in PLAN:
-        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        n, d = int(color[:5], 16), int(color[-1])
+    for d, n, color in PLAN:
+        if pt2:
+            n, d = int(color[:5], 16), int(color[-1])
         perim += n
         di, dj = dirs[d]
         i += n * di
@@ -47,5 +39,5 @@ def part_two():
     return shoelace_area(points) + (perim >> 1) + 1
 
 
-print(f"Part 1: {part_one()}")  # 47675
-print(f"Part 2: {part_two()}")  # 122103860427465
+print(f"Part 1: {solve()}")  # 47675
+print(f"Part 2: {solve(True)}")  # 122103860427465
