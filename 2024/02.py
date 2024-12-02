a4534675fb87fa8b6e8a6e7982e1a9fea783a058
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2024-12-02 00:38:25 -0500

    Add 2024 day 02

---
+++ 2024/02.py
@@ -0,0 +1,28 @@
+from itertools import pairwise
+
+with open("02.txt", "r") as file:
+    data = file.read().strip().splitlines()
+
+
+REPORTS = [[int(x) for x in row.split()] for row in data]
+
+
+def check(report):
+    return all(1 <= (b - a) <= 3 for a, b in pairwise(report)) or all(
+        1 <= (a - b) <= 3 for a, b in pairwise(report)
+    )
+
+
+def part_one():
+    return sum(check(report) for report in REPORTS)
+
+
+def part_two():
+    return sum(
+        any(check(report[:i] + report[i + 1 :]) for i in range(len(report)))
+        for report in REPORTS
+    )
+
+
+print(f"Part 1: {part_one()}")
+print(f"Part 2: {part_two()}")
