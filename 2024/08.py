30f48d37d3996a3719ad2587f58876340044822c
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2024-12-08 00:35:49 -0500

    Add 2024 day 08

---
+++ 2024/08.py
@@ -0,0 +1,64 @@
+from collections import defaultdict
+from itertools import combinations
+
+
+def parse_input():
+    with open("08.txt", "r") as file:
+        data = file.read()
+    grid = data.splitlines()
+    antennas = defaultdict(list)
+    for i, row in enumerate(grid):
+        for j, v in enumerate(row):
+            if v != ".":
+                antennas[v].append((i, j))
+    return antennas, len(grid), len(grid[0])
+
+
+ANTENNAS, N, M = parse_input()
+
+
+def calc_antinodes(u, v, pt2=False):
+    ui, uj = u
+    vi, vj = v
+    di = ui - vi
+    dj = uj - vj
+    res = {(ui, uj), (vi, vj)} if pt2 else set()
+    while 0 <= ui + di < N and 0 <= uj + dj < M:
+        ui += di
+        uj += dj
+        res |= {(ui, uj)}
+        if not pt2:
+            break
+    while 0 <= vi - di < N and 0 <= vj - dj < M:
+        vi -= di
+        vj -= dj
+        res |= {(vi, vj)}
+        if not pt2:
+            break
+    return res
+
+
+def part_one():
+    return len(
+        {
+            antinode
+            for locs in ANTENNAS.values()
+            for u, v in combinations(locs, 2)
+            for antinode in calc_antinodes(u, v)
+        }
+    )
+
+
+def part_two():
+    return len(
+        {
+            antinode
+            for locs in ANTENNAS.values()
+            for u, v in combinations(locs, 2)
+            for antinode in calc_antinodes(u, v, True)
+        }
+    )
+
+
+print(f"Part 1: {part_one()}")
+print(f"Part 2: {part_two()}")
