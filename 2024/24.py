6e2f2ca6f5f43827d403fccaed89d51cfdf58746
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2024-12-24 13:47:05 -0500

    Refactor part 2 into an unnecessary and unreadable comprehension

--- 2024/24.py
+++ 2024/24.py
@@ -30,31 +30,34 @@ def part_one():
 
 
 def part_two():
-    res = [
-        c
-        for c, (a, op, b) in GATES.items()
-        if (
-            (c.startswith("z") and op != "XOR" and c != "z45")
-            or (
-                op == "XOR"
-                and all(not x.startswith(("x", "y", "z")) for x in (a, b, c))
-            )
-            or (
-                op == "AND"
-                and "x00" not in (a, b)
-                and any(
-                    c in (aa, bb) and op2 != "OR" for (aa, op2, bb) in GATES.values()
+    return ",".join(
+        sorted(
+            c
+            for c, (a, op, b) in GATES.items()
+            if (
+                (c.startswith("z") and op != "XOR" and c != "z45")
+                or (
+                    op == "XOR"
+                    and all(not x.startswith(("x", "y", "z")) for x in (a, b, c))
                 )
-            )
-            or (
-                op == "XOR"
-                and any(
-                    c in (aa, bb) and op2 == "OR" for (aa, op2, bb) in GATES.values()
+                or (
+                    op == "AND"
+                    and "x00" not in (a, b)
+                    and any(
+                        c in (aa, bb) and op2 != "OR"
+                        for (aa, op2, bb) in GATES.values()
+                    )
+                )
+                or (
+                    op == "XOR"
+                    and any(
+                        c in (aa, bb) and op2 == "OR"
+                        for (aa, op2, bb) in GATES.values()
+                    )
                 )
             )
         )
-    ]
-    return ",".join(sorted(res))
+    )
 
 
 print(f"Part 1: {part_one()}")
