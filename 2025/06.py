5a3cfbe2330ef14415d297b2b42223956b78d2ec
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2025-12-06 09:01:11 -0500

    Add 2025 day 06

---
+++ 2025/06.py
@@ -0,0 +1,40 @@
+from functools import reduce
+from itertools import pairwise
+from operator import add, mul
+
+
+def parse_input():
+    with open("06.txt", "r") as f:
+        data = f.read().splitlines()
+    return data[:-1], data[-1]
+
+
+NUMS, OPS = parse_input()
+
+
+def part_one():
+    return sum(
+        reduce(op, col)
+        for op, col in zip(
+            [{"*": mul, "+": add}[x] for x in OPS.split()],
+            zip(*(map(int, row.split()) for row in NUMS)),
+        )
+    )
+
+
+def part_two():
+    term = [i for i, v in enumerate(OPS) if v != " "] + [len(OPS) + 1]
+    padded = [
+        [row[i : j - 1].replace(" ", "0") for i, j in pairwise(term)] for row in NUMS
+    ]
+    return sum(
+        reduce(op, col)
+        for op, col in zip(
+            [{"*": mul, "+": add}[x] for x in OPS.split()],
+            [[int("".join(c).rstrip("0")) for c in zip(*col)] for col in zip(*padded)],
+        )
+    )
+
+
+print(f"Part 1: {part_one()}")
+print(f"Part 2: {part_two()}")
